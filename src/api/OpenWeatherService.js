3a9df1533d3ad3998887c741af0a1b7afcc63a0c
Akash Gautam <akash.gautam2024@nst.rishihood.edu.in>
2025-04-28 23:35:23 +0530

    Update OpenWeatherService.js

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'd6c77ee285aced4b0802a57a1857ca6f';
+const WEATHER_API_KEY = '78d9aee10cdc9f7c000703b4b0920d4b';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
